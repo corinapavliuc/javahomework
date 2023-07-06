f353bf2633ecbe2e6c166e74b118d8f77e350686
corinapavliuc <138014725+corinapavliuc@users.noreply.github.com>
2023-07-07 00:09:22 +0200

    Add files via upload
    java home-work

---
+++ Main.java
@@ -0,0 +1,25 @@
+package homework;
+
+public class Main {
+    public static void main(String[] args) {
+
+        char ch = 'G';
+        int num = 89;
+        byte numm =4;
+        short nummm =56;
+        float na = (float) 4.7333436;
+        double ca= 4.355453532;
+        long ma =12121;
+
+        System.out.println("Character: " + "G");
+        System.out.println("Integer: " + "89");
+        System.out.println("Byte: " + "4");
+        System.out.println("Short: " + "56");
+        System.out.println("float: " + "4.7333436");
+        System.out.println("Double: " + "4.355453532");
+        System.out.println("Long: "+ "12121");
+
+
+
+    }
+}
